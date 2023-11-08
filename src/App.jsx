b87fef80f4ba1e7f5559e98dd3bbd60692b2391f
kwbyron <kbyron11@gmail.com>
2023-11-08 15:43:22 -0800

    add outline to App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,58 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
-
   return (
     <>
-      <h1>Hello!</h1>
+      <Main>
+        <Inputs>
+          <Bill />
+          <SelectTipPercent />
+          <NumberOfPeople />
+        </Inputs>
+        <Outputs>
+          <TipAmountPerPerson />
+          <TotalPerPerson />
+          <Reset />
+        </Outputs>
+      </Main>
     </>
-  )
+  );
 }
 
-export default App
+function Main() {
+  return <div className="main"></div>;
+}
+
+function Inputs() {
+  return <div className="inputs-box"></div>;
+}
+
+function Bill() {
+  return <div className="input-bill"></div>;
+}
+
+function SelectTipPercent() {
+  return <div className="input-tip"></div>;
+}
+
+function NumberOfPeople() {
+  return <div className="input-people"></div>;
+}
+
+function Outputs() {
+  return <div className="outputs-box"></div>;
+}
+
+function TipAmountPerPerson() {
+  return <div className="output-tip"></div>;
+}
+
+function TotalPerPerson() {
+  return <div className="output-total"></div>;
+}
+
+function Reset() {
+  return <button className="btn-reset"></button>;
+}
+
+export default App;
